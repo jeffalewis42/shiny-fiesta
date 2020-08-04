ebdde0334b4d0256574aad6ac1b629abd0f9b467
Jeffrey Lewis <jeffalewis42@gmail.com>
2020-08-04 15:43:50 -0500

    with ingredients

--- server.js
+++ server.js
@@ -1,10 +1,50 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+
+var ingredients = [
+    {
+        "id": "232kjk",
+        "text": "Eggs"
+    },
+    {
+        "id": "klkjkj",
+        "text": "Milk"
+    },
+    {
+        "id": "bmbmsjk",
+        "text": "Bread"
+    },
+    {
+        "id": "2335ff",
+        "text": "Pepperoni Pizza"
+    }
+    ];
+
 
 app.get('/', function (request, response) {
-    response.send('My First API');
+    response.send(ingredients);
 
 });
+
+app.post('/', function (request, response) {
+    var ingredient = request.body;
+    if (!ingredient || ingredient.text === "") {
+        response.status(500).send({
+            error: "Your Ingredient must have text"
+        });
+    } else {
+        ingredients.push(ingredient);
+        response.status(200).send(ingredients);
+    }
+})
 
 app.get('/funyuns', function (request, response) {
     response.send('My First API with more flavor');
